205c2162a0b3447992c079ab7bd29c19339e3823
sid-thakur47 <sidthakur258@gmail.com>
2020-04-18 21:34:16 +0530

    ADDED:Functionality to test multiply and addition operation

--- src/main/java/com/bl/demo/ArithmeticComputation.java
+++ src/main/java/com/bl/demo/ArithmeticComputation.java
@@ -1,6 +1,10 @@
 package com.bl.demo;
 
 public class ArithmeticComputation {
+    public int multiplyAdd(int a, int b, int c) {
+        return (a * b) + c;
+    }
+
     public class Arithmetic {
         public int addMultiply(int a, int b, int c) {
             return a + (b * c);
--- src/test/java/com/bl/demo/ArithmeticComputationTest.java
+++ src/test/java/com/bl/demo/ArithmeticComputationTest.java
@@ -4,10 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ArithmeticComputationTest {
+    Arithmetic arithmetic = new Arithmetic();
     @Test
     public void givenTwoValue_WhenAddAndMultiply_ReturnAddMultiply() {
-        Arithmetic arithmetic = new Arithmetic();
+
         int addMultiplyResult = arithmetic.addMultiply(2, 2, 2);
         Assert.assertEquals(6, addMultiplyResult);
+    }
+
+    @Test
+    public void givenTwoValue_WhenMultiplyAdd_ReturnMultiplyAdd() {
+        int multiplyAddResult = arithmetic.multiplyAdd(2, 4, 1);
+        Assert.assertEquals(9, multiplyAddResult);
     }
 }
